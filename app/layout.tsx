5f34e6c2b0fba5e54747ece689d12d61fe0e133d
bret <bretai@LCARS-MX-23>
2024-06-18 15:15:19 +0300

    Update site description and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import ToastProvider from "@/components/providers/toaster-provider";
-import "./globals.css";
+import ConfettiProvider from "@/provider/confetti-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import ConfettiProvider from "@/provider/confetti-provider";
 import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LMS",
+  title: "Learning Anything, On Your Schedule | LMS",
   description: "Learning Management System",
 };
 
